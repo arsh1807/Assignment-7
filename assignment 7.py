56956d0215a05f5f758f295331442b2511bbfc8a
arsh1807 <arshi.1807@gmail.com>
2018-08-26 18:29:10 +0530

    this is my seventh assignment

---
+++ assignment 7.py
@@ -0,0 +1,15 @@
+#ques 1
+dict={"apple":"green","banana":"yellow","cherry": "red"}
+list=[]
+for i in dict.keys():
+    list.append(i)
+print(list)
+
+#ques 2
+
+people = {'Arshi': {'maths':'99', 'english': '99', 'science': '99'},
+          'Arpit': {'maths':'33', 'english': '33', 'science': '33'}}
+print(people['Arshi']['maths'],people['Arshi']['english'],people['Arshi']['science'])
+print(people['Arpit']['maths'],people['Arpit']['english'],people['Arpit']['science'])
+
+
